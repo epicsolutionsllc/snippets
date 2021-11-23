326f16b1423ef4c9d919085f0e8233ab4ac74672
Micah Lindley <micahincorporated@gmail.com>
2021-11-23 15:58:53 -0600

    Account for navigation inside the webview

--- js/mobilepages.js
+++ js/mobilepages.js
@@ -1,3 +1,11 @@
+const previous = document.referrer;
+if (previous.includes("?app")) {
+  if (window.location.href.includes("?")) {
+    history.pushState({}, "Sureway", window.location.href + "&app=true")
+  } else {
+    history.pushState({}, "Sureway", window.location.href + "?app=true")
+  }
+}
 const urlParams = new URLSearchParams(window.location.search);
 const isMobile = urlParams.get('app');
 if (isMobile == "true" || isMobile == "yes" || isMobile == "") {
